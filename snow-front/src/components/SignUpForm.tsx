4cab38bf8e60156ba00dec041c69b3f3a6ec1da7
Tetiana Rakova <tatyrakova2002@gmail.com>
2021-12-21 21:55:26 +0200

    add sign up level field

--- snow-front/src/components/SignUpForm.tsx
+++ snow-front/src/components/SignUpForm.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import { signUp } from "../Utils/Api";
+import { Level } from "../types";
+import { getLevels, signUp } from "../Utils/Api";
 
 class Input {
   username: string = "";
   email: string = "";
-  level_id: string = "";
+  level: Level = { _id: "", name: "" };
   password: string = "";
   confirm_password: string = "";
   agreement: boolean = false;
@@ -24,18 +26,47 @@ class NewUserData {
   password: string = "";
 }
 
+function getLevelName(id: string, a: Array<Level>): string {
+  for (let x = 0; x < a.length; x++) {
+    console.log(x + " .) " + a[x]["_id"] + " -> " + id);
+    if (a[x]["_id"] == id) {
+      // console.log(x.name);
+      return a[x].name;
+    }
+  }
+  console.log("WRONG CODE!!!!!!!");
+  return "";
+}
+
 function SignUpForm() {
   const navigate = useNavigate();
   const [input, setValues] = useState(new Input());
   const [errors, setErrors] = useState(new Error());
+  const [levels, setLevels] = useState(new Array<Level>());
+
+  useLayoutEffect(() => {
+    async function fetchApi() {
+      const levels: Level[] = await getLevels();
+      setLevels(levels);
+      console.log("------");
+      console.log(levels);
+    }
+    fetchApi();
+  }, []);
 
   const handleSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
 
     if (validate()) {
       const data: Input = input as Input;
-
-      const result = await signUp(data);
+      const new_data = {
+        username: data.username,
+        password: data.password,
+        confirm_password: data.confirm_password,
+        email: data.email,
+        level_id: data.level._id,
+      };
+      const result = await signUp(new_data);
       if (result.error) {
         setErrors({ confirm_password: "", message: result.error });
       } else {
@@ -59,7 +90,7 @@ function SignUpForm() {
     }
     return isValid;
   };
-
+  const { t } = useTranslation();
   return (
     <Form onSubmit={handleSubmit}>
       <div className="text-danger">
@@ -79,7 +110,7 @@ function SignUpForm() {
           placeholder="Enter name"
         />
       </Form.Group>
-      
+
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control
@@ -97,19 +128,35 @@ function SignUpForm() {
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicSurname">
-        <Form.Label>Level</Form.Label>
-        <Form.Control
-          id="level_id"
-          name="Level_id"
-          value={input.level_id}
+      <Form.Group className="mb-3" controlId="formBasicUnit">
+        <Form.Label>{t("Level")}</Form.Label>
+        <Form.Select
+          id="levels"
+          name="levels"
+          value={input.level._id}
           onChange={(e) => {
-            setValues({ ...input, level_id: e.target.value });
+            setValues({
+              ...input,
+              level: {
+                _id: e.target.value,
+                name: getLevelName(e.target.value, levels),
+              },
+            });
           }}
           required
-          type="text"
-          placeholder="Select level"
-        />
+          placeholder={t("Enter level")}
+        >
+          {levels.map((level) => (
+            <option
+              selected={input.level == { _id: level._id, name: level.name }}
+              value={level._id}
+              id={level._id}
+              key={level.name}
+            >
+              {level.name}
+            </option>
+          ))}
+        </Form.Select>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
